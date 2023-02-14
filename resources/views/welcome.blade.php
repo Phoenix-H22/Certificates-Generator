e7bdb8f63dc96a5038c6bbae215472807a463635
Abdalrhman M. Alkady <alkady2019@gmail.com>
2023-02-14 04:35:32 +0200

    edits

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -97,9 +97,7 @@
     <!-- For more icon options, visit https://fontawesome.com/icons?d=gallery&p=2&s=brands-->
     <div class="social-icons">
         <div class="d-flex flex-row flex-lg-column justify-content-center align-items-center h-100 mt-3 mt-lg-0">
-            <a class="btn btn-dark m-3" href="#!"><i class="fab fa-twitter"></i></a>
-            <a class="btn btn-dark m-3" href="#!"><i class="fab fa-facebook-f"></i></a>
-            <a class="btn btn-dark m-3" href="#!"><i class="fab fa-instagram"></i></a>
+            <a class="btn btn-dark m-3" href="https://www.facebook.com/asfec.unesco.3"><i class="fab fa-facebook-f"></i></a>
             <a class="btn btn-dark m-3" href="{{url("login")}}">Login</a>
             <a class="btn btn-dark m-3" href="{{url("register")}}">Register</a>
 
